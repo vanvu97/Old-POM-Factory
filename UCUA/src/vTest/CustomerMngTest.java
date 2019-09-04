dc94fc06300381f9188bff683360e543528ca948
Van Vu <vuvan@dicentral.vn>
2019-09-04 19:23:14 +0700

    Update 09/04/2019

--- UCUA/src/vTest/CustomerMngTest.java
+++ UCUA/src/vTest/CustomerMngTest.java
@@ -9,32 +9,30 @@ import vPages.CustomerMngPage;
 import vPages.UCLoginPage;
 import vUtilyty.BaseTest;
 import vUtilyty.ExportFile;
-import vUtilyty.Links;
 
 public class CustomerMngTest extends BaseTest {
   
 	UCLoginPage loginUC;
 	CustomerMngPage MngPgs;
 	
-	@Test
-  public void CreateAccount() throws InterruptedException, FileNotFoundException {
+	@Test(dataProvider="loginData")
+  public void CreateAccount(String userName, String password) throws InterruptedException, FileNotFoundException {
 		loginUC = new UCLoginPage(driver);
 		MngPgs = new CustomerMngPage(driver);
 		
-		driver.get(Links.URL_UC);
-		loginUC.LoginUC("admin", "123456"); 
+		loginUC.LoginUC(userName, password);
 		MngPgs.reachToCustomerSection();
 //		MngPgs.AddCustomer();
 //		MngPgs.PrintInfo();
 		MngPgs.setPassword();
-  }
-	@DataProvider
-	public Object[][] Authentication() throws Exception{
-		String filePath = System.getProperty("user.dir")+"\\src\\vUtilyty\\";
+	}
+	@DataProvider(name = "loginData")
+	public Object[][] dataProvider() throws Exception {
+		String filePath = System.getProperty("user.dir")+"\\src\\data\\";
 		
 	     Object[][] testObjArray = ExportFile.getTableArray(filePath + "ExportExcel.xlsx","LoginData", 0);
 
 	     return (testObjArray);
-
-		}
+	}
 }
+
