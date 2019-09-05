354d21c180f9e5ea247255df3fc8d7cd44e99ad7
Van Vu <vuvan@dicentral.vn>
2019-09-05 15:53:19 +0700

    Update 09/05/2019

---
+++ UCProject/src/vTest/CustomerMngTest.java
@@ -0,0 +1,38 @@
+package vTest;
+
+import java.io.FileNotFoundException;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import vPages.CustomerMngPage;
+import vPages.UCLoginPage;
+import vUtilyty.BaseTest;
+import vUtilyty.ExportFile;
+
+public class CustomerMngTest extends BaseTest {
+  
+	UCLoginPage loginUC;
+	CustomerMngPage MngPgs;
+	
+	@Test(dataProvider="loginData")
+  public void CreateAccount(String userName, String password,String sEmail) throws InterruptedException, FileNotFoundException {
+		loginUC = new UCLoginPage(driver);
+		MngPgs = new CustomerMngPage(driver);
+		
+		loginUC.LoginUC(userName, password);
+		MngPgs.reachToCustomerSection();
+//		MngPgs.AddCustomer();
+//		MngPgs.PrintInfo();
+		MngPgs.setPassword();
+	}
+	@DataProvider(name = "loginData")
+	public Object[][] dataProvider() throws Exception {
+		String filePath = System.getProperty("user.dir")+"\\src\\data\\";
+		
+	     Object[][] testObjArray = ExportFile.getTableArray(filePath + "ExportExcel.xlsx","LoginData", 0);
+
+	     return (testObjArray);
+	}
+}
+
